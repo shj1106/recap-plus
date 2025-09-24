a147b7bd97cc5945d8989b248970017d5eb10ae3
sahyunjin <tkguswls1106@naver.com>
2025-09-25 03:23:29 +0900

    Fix: ArrayList 임포트 에러 해결

--- src/main/java/org/recap/Summarizer.java
+++ src/main/java/org/recap/Summarizer.java
@@ -6,6 +6,7 @@ import org.recap.graph.WeightedGraph;
 import org.recap.sentence.Extractor;
 import org.recap.sentence.Splitter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
